ab6b1119b18d33b4ec7e1929dd32bae886be361d
Emir Güney <43078754+emirguney@users.noreply.github.com>
2021-08-14 20:30:27 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-    root :to => 'index#index'
+    root :to => 'application#application'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
