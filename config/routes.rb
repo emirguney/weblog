86041e908a9999b98dea1253159e7aef38c97c38
Emir Güney <43078754+emirguney@users.noreply.github.com>
2021-08-14 20:03:55 +0300

    db

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-    root :to => 'Applicaiton#Application'
+    root :to => 'index#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
